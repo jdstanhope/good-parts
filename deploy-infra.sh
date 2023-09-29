5fd24eb5c0900814fc9f3d20789364b5ea2d8a0c
John Stanhope <jdstanhope@gmail.com>
2023-09-29 10:41:27 -0500

    add codebuild / codeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -2,6 +2,8 @@ STACK_NAME=good-parts
 REGION=us-east-1
 CLI_PROFILE=default
 EC2_INSTANCE_TYPE=t2.micro
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 echo -e "\n\n\nDeploying main.yml\n"
 aws cloudformation deploy \
@@ -12,6 +14,7 @@ aws cloudformation deploy \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
+        CodePipelineBucket=$CODEPIPELINE_BUCKET \
         EC2InstanceType=$EC2_INSTANCE_TYPE
 
 if [ $? -eq 0 ]; then
