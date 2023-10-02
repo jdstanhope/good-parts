63cac1541505be162dc3f2fa46fe9272c41ff262
John Stanhope <jdstanhope@gmail.com>
2023-10-02 15:19:34 -0500

    almost works, something still wrong with the pipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,6 +5,12 @@ EC2_INSTANCE_TYPE=t2.micro
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+# These values come from the env
+# GH_ACESS_TOKEN
+# GH_OWNER
+# GH_REPO
+# GH_BRANCH
+
 echo -e "\n\n\nDeploying main.yml\n"
 aws cloudformation deploy \
     --region $REGION \
@@ -14,8 +20,12 @@ aws cloudformation deploy \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-        CodePipelineBucket=$CODEPIPELINE_BUCKET \
-        EC2InstanceType=$EC2_INSTANCE_TYPE
+        EC2InstanceType=$EC2_INSTANCE_TYPE \
+        GitHubOwner=$GH_OWNER \
+        GitHubRepo=$GH_REPO \
+        GitHubBranch=$GH_BRANCH \
+        GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+        CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 if [ $? -eq 0 ]; then
     aws cloudformation list-exports \
