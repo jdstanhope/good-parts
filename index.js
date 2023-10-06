89632bdc77f11ca440a1e4326e54a3f5f6992e3c
John Stanhope <jdstanhope@gmail.com>
2023-10-06 11:25:28 -0500

    report stack name in output

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { hostname } = require('os');
 const http = require('http');
-
+const STACK_NAME = process.env.STACK_NAME || 'unknown stack';
+const message = `Hello World from ${hostname()} in ${STACK_NAME}\n`;
 const port = 8081;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(`Hello World from ${hostname()}\n`);
+    res.end(message);
 });
 
 server.listen(port, hostname, () => {
--- start-service.sh
+++ start-service.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
+
+REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
+export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+export STACK_NAME=`aws --region $REGION ec2 describe-tags \
+    --filters "Name=resource-id,Values=${INSTANCE_ID}" \
+    | jq -r ".Tags[0].Value"`
+
 npm run start
